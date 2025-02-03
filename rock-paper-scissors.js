348a5f39b579ea551b2524058986c62f6205f449
alexnoy <alexander.noy123@gmail.com>
2025-02-02 23:33:24 -0500

    Initialize varibles for player and comp choices.
    Created the required variables to hold the players and computers choices for the playRound function.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,7 @@
 let computerScore = 0;
 let humanScore = 0;
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
 
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
@@ -17,7 +19,7 @@ function getHumanChoice() {
     let choice = prompt("Select one: rock, paper, scissors");
 
     if (choice.toLowerCase() != "rock" && choice.toLowerCase() != "paper" && choice.toLowerCase() != "scissors") {
-        return "Selection incorrect refresh to select again."
+        return "Selection incorrect refresh to select again.";
     }else {
         return choice.toLowerCase();
     }
