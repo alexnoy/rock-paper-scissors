c2bb862179c88f06ff9fa192d7a88c07fddb9d9c
alexnoy <alexander.noy123@gmail.com>
2025-02-02 23:35:11 -0500

    Create playRound function
    Created the playRound function that plays a round and compares the choices and decides a winner.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -24,3 +24,29 @@ function getHumanChoice() {
         return choice.toLowerCase();
     }
 }
+
+function playRound(humanChoice,computerChoice) {
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        ++humanScore;
+        return "Player wins. rock beats scissors";
+    }else if (humanChoice == "rock" && computerChoice == "paper") {
+        ++computerScore;
+        return "Computer wins. paper beats rock";
+    }else if (humanChoice == "paper" && computerChoice == "rock") {
+        ++humanScore;
+        return "Player wins. paper beats rock";
+    }else if (humanChoice == "paper" && computerChoice == "scissors") {
+        ++computerScore;
+        return "Computer wins. scissors beats paper";
+    }else if (humanChoice == "scissors" && computerChoice == "paper") {
+        ++humanScore;
+        return "Player wins. scissors beats paper";
+    }else if (humanChoice == "scissors" && computerChoice == "rock") {
+        ++computerScore;
+        return "Computer wins. rock beats scissors";
+    }else {
+        return "Tie. choices were the same."
+    }
+}
+
+console.log(playRound(humanChoice,computerChoice));
