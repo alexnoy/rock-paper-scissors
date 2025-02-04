471b7c02bc5cc65bc6775b7ea2647c980f3c0bbb
alexnoy <alexander.noy123@gmail.com>
2025-02-03 22:09:35 -0500

    Create playGame function
    Created the playGame function and moved the rest of the code inside.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,52 +1,59 @@
-let computerScore = 0;
-let humanScore = 0;
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
+function playGame() {
+    let computerScore = 0;
+    let humanScore = 0;
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
 
-function getComputerChoice() {
-    let num = Math.floor(Math.random() * 3);
+    function getComputerChoice() {
+        let num = Math.floor(Math.random() * 3);
+    
+        if (num == 0) {
+            return "rock";
+        }else if (num == 1) {
+            return "paper";
+        }else {
+            return "scissors";
+        }
+    }
+    
+    function getHumanChoice() {
+        let choice = prompt("Select one: rock, paper, scissors");
+    
+        if (choice.toLowerCase() != "rock" && choice.toLowerCase() != "paper" && choice.toLowerCase() != "scissors") {
+            return "Selection incorrect refresh to select again.";
+        }else {
+            return choice.toLowerCase();
+        }
+    }
 
-    if (num == 0) {
-        return "rock";
-    }else if (num == 1) {
-        return "paper";
-    }else {
-        return "scissors";
+    function playRound(humanChoice,computerChoice) {
+        if (humanChoice == "rock" && computerChoice == "scissors") {
+            ++humanScore;
+            console.log("Player score:", humanScore, "Computer score:", computerScore);
+            return "Player wins. rock beats scissors";
+        }else if (humanChoice == "rock" && computerChoice == "paper") {
+            ++computerScore;
+            console.log("Player score:", humanScore, "Computer score:", computerScore);
+            return "Computer wins. paper beats rock";
+        }else if (humanChoice == "paper" && computerChoice == "rock") {
+            ++humanScore;
+            console.log("Player score:", humanScore, "Computer score:", computerScore);
+            return "Player wins. paper beats rock";
+        }else if (humanChoice == "paper" && computerChoice == "scissors") {
+            ++computerScore;
+            console.log("Player score:", humanScore, "Computer score:", computerScore);
+            return "Computer wins. scissors beats paper";
+        }else if (humanChoice == "scissors" && computerChoice == "paper") {
+            ++humanScore;
+            console.log("Player score:", humanScore, "Computer score:", computerScore);
+            return "Player wins. scissors beats paper";
+        }else if (humanChoice == "scissors" && computerChoice == "rock") {
+            ++computerScore;
+            console.log("Player score:", humanScore, "Computer score:", computerScore);
+            return "Computer wins. rock beats scissors";
+        }else {
+            console.log("Player score:", humanScore, "Computer score:", computerScore);
+            return "Tie. choices were the same.";
+        }
     }
 }
-
-function getHumanChoice() {
-    let choice = prompt("Select one: rock, paper, scissors");
-
-    if (choice.toLowerCase() != "rock" && choice.toLowerCase() != "paper" && choice.toLowerCase() != "scissors") {
-        return "Selection incorrect refresh to select again.";
-    }else {
-        return choice.toLowerCase();
-    }
-}
-
-function playRound(humanChoice,computerChoice) {
-    if (humanChoice == "rock" && computerChoice == "scissors") {
-        ++humanScore;
-        return "Player wins. rock beats scissors";
-    }else if (humanChoice == "rock" && computerChoice == "paper") {
-        ++computerScore;
-        return "Computer wins. paper beats rock";
-    }else if (humanChoice == "paper" && computerChoice == "rock") {
-        ++humanScore;
-        return "Player wins. paper beats rock";
-    }else if (humanChoice == "paper" && computerChoice == "scissors") {
-        ++computerScore;
-        return "Computer wins. scissors beats paper";
-    }else if (humanChoice == "scissors" && computerChoice == "paper") {
-        ++humanScore;
-        return "Player wins. scissors beats paper";
-    }else if (humanChoice == "scissors" && computerChoice == "rock") {
-        ++computerScore;
-        return "Computer wins. rock beats scissors";
-    }else {
-        return "Tie. choices were the same."
-    }
-}
-
-console.log(playRound(humanChoice,computerChoice));
