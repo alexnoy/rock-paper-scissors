7cf4b0f4213c38e7b6175302ce4dc9a945fa9c3a
alexnoy <alexander.noy123@gmail.com>
2025-03-17 22:05:45 -0400

    Rework getComputerChoice, remove getPlayerChoice
    Changed how the computer makes its selection and added functionality to allow players to click on buttons to make their selection.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,66 +1,48 @@
+function getComputerChoice() {
+    let choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+let computerScore = 0;
+let humanScore = 0;
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', function(e){
+    playRound(e.target.id, getComputerChoice());
+}));
+
+function playRound(humanChoice,computerChoice) {
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        ++humanScore;
+        console.log("Player score:", humanScore, "Computer score:", computerScore);
+        return "Player wins. rock beats scissors";
+    }else if (humanChoice == "rock" && computerChoice == "paper") {
+        ++computerScore;
+        console.log("Player score:", humanScore, "Computer score:", computerScore);
+        return "Computer wins. paper beats rock";
+    }else if (humanChoice == "paper" && computerChoice == "rock") {
+        ++humanScore;
+        console.log("Player score:", humanScore, "Computer score:", computerScore);
+        return "Player wins. paper beats rock";
+    }else if (humanChoice == "paper" && computerChoice == "scissors") {
+        ++computerScore;
+        console.log("Player score:", humanScore, "Computer score:", computerScore);
+        return "Computer wins. scissors beats paper";
+    }else if (humanChoice == "scissors" && computerChoice == "paper") {
+        ++humanScore;
+        console.log("Player score:", humanScore, "Computer score:", computerScore);
+        return "Player wins. scissors beats paper";
+    }else if (humanChoice == "scissors" && computerChoice == "rock") {
+        ++computerScore;
+        console.log("Player score:", humanScore, "Computer score:", computerScore);
+        return "Computer wins. rock beats scissors";
+    }else {
+        console.log("Player score:", humanScore, "Computer score:", computerScore);
+        return "Tie. choices were the same.";
+    }
+}
+
 function playGame() {
-    let computerScore = 0;
-    let humanScore = 0;
-
-    function getComputerChoice() {
-        let num = Math.floor(Math.random() * 3);
-    
-        if (num == 0) {
-            return "rock";
-        }else if (num == 1) {
-            return "paper";
-        }else {
-            return "scissors";
-        }
-    }
-    
-    function getHumanChoice() {
-        let choice = prompt("Select one: rock, paper, scissors");
-    
-        if (choice.toLowerCase() != "rock" && choice.toLowerCase() != "paper" && choice.toLowerCase() != "scissors") {
-            throw new Error("Selection incorrect refresh to start again.");
-        }else {
-            return choice.toLowerCase();
-        }
-    }
-
-    function playRound(humanChoice,computerChoice) {
-        if (humanChoice == "rock" && computerChoice == "scissors") {
-            ++humanScore;
-            console.log("Player score:", humanScore, "Computer score:", computerScore);
-            return "Player wins. rock beats scissors";
-        }else if (humanChoice == "rock" && computerChoice == "paper") {
-            ++computerScore;
-            console.log("Player score:", humanScore, "Computer score:", computerScore);
-            return "Computer wins. paper beats rock";
-        }else if (humanChoice == "paper" && computerChoice == "rock") {
-            ++humanScore;
-            console.log("Player score:", humanScore, "Computer score:", computerScore);
-            return "Player wins. paper beats rock";
-        }else if (humanChoice == "paper" && computerChoice == "scissors") {
-            ++computerScore;
-            console.log("Player score:", humanScore, "Computer score:", computerScore);
-            return "Computer wins. scissors beats paper";
-        }else if (humanChoice == "scissors" && computerChoice == "paper") {
-            ++humanScore;
-            console.log("Player score:", humanScore, "Computer score:", computerScore);
-            return "Player wins. scissors beats paper";
-        }else if (humanChoice == "scissors" && computerChoice == "rock") {
-            ++computerScore;
-            console.log("Player score:", humanScore, "Computer score:", computerScore);
-            return "Computer wins. rock beats scissors";
-        }else {
-            console.log("Player score:", humanScore, "Computer score:", computerScore);
-            return "Tie. choices were the same.";
-        }
-    }
-
-    for (i = 0; i < 5; i++) {
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        console.log(playRound(humanChoice,computerChoice));
-    }
-
     if (humanScore > computerScore) {
         console.log("Total player score:", humanScore, "total Computer score:", computerScore);
         return "Player Wins!!"
@@ -72,5 +54,3 @@ function playGame() {
         return "Tie Game!"
     }
 }
-
-console.log(playGame());
